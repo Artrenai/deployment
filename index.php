89f5e54f56391384d342131e57ea66c45b15f439
Godfrey Davis <gdavis42@cnm.edu>
2017-08-08 07:01:31 -0600

    reformatted code

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel = "stylesheet" type = "text/css" href = "css/styles.css" />
+		<link rel="stylesheet" type="text/css" href="css/styles.css"/>
 		<script src="js/script.js"></script>
 	</head>
-	<body><div id="content">
+	<body>
+		<div id="content">
 			My name is <span id="name">Godfrey</span>
 		</div>
 		<button type="button" onclick="changeColor();">Change Color</button>
 		<button type="button" onclick="changeFont();">Change Font</button>
 		<div>
 			<label for="newName">Whats your name?</label>
-			<input type="text" id="newName" name="newName" onchange="newName();" onkeyup="newName();" />
+			<input type="text" id="newName" name="newName" onchange="newName();" onkeyup="newName();"/>
 		</div>
 	</body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-function changeColor(){
-	document.getElementById("content").style.color ="red";
+function changeColor() {
+	document.getElementById("content").style.color = "red";
 }
 
-function changeFont(){
-	document.getElementById("content").style.fontFamily ="Impact";
+function changeFont() {
+	document.getElementById("content").style.fontFamily = "Impact";
 }
 
-function newName(){
-		document.getElementById("name").innerHTML = document.getElementById("newName").value;
+function newName() {
+	document.getElementById("name").innerHTML = document.getElementById("newName").value;
 
 }
