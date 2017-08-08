e570a84a486f24f2fe23f6c3c86f9e86a0e30066
Godfrey Davis <gdavis42@cnm.edu>
2017-08-08 07:00:43 -0600

    finished challenge

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel = "stylesheet" type = "text/css" href = "css/styles.css" />
+		<script src="js/script.js"></script>
+	</head>
+	<body><div id="content">
+			My name is <span id="name">Godfrey</span>
+		</div>
+		<button type="button" onclick="changeColor();">Change Color</button>
+		<button type="button" onclick="changeFont();">Change Font</button>
+		<div>
+			<label for="newName">Whats your name?</label>
+			<input type="text" id="newName" name="newName" onchange="newName();" onkeyup="newName();" />
+		</div>
+	</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,12 @@
+function changeColor(){
+	document.getElementById("content").style.color ="red";
+}
+
+function changeFont(){
+	document.getElementById("content").style.fontFamily ="Impact";
+}
+
+function newName(){
+		document.getElementById("name").innerHTML = document.getElementById("newName").value;
+
+}
