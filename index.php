3403f58de593fe3b53d643c5beacfac927f3148a
Godfrey Davis <gdavis42@cnm.edu>
2017-08-10 07:05:34 -0600

    added documentation

--- index.php
+++ index.php
@@ -11,17 +11,20 @@
 			name within the text below! You can change the color of said text <strong>5.... whole... Times!!!</strong>Also,
 			It can change the font to Five rediculously Amazing fonts!
 		</p>
+		<!--the information being changed by events -->
 		<p id="content">
 			My name is <span id="name">Godfrey</span>
 		</p>
-
+		<!--color will change when button is pressed-->
 		<button id="changeColorButton">Change Color</button>
+		<!--font will change when button is pressed-->
 		<button id="changeFontButton">Change Font</button>
-
+		<!---user input to dynamically change Name-->
 		<div>
 			<label for="newName">Whats your name?</label>
 			<input type="text" id="newName" name="newName" onchange="newName();" onkeyup="newName();"/>
 		</div>
+		<!-- had to put script at bottom because java script is depedent on events and values specifed in html -->
 		<script src="js/script.js"></script>
 	</body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 var Request;
+//counts created to hold the value of how many clicks
 var counter = 0;
 var counterS = 0;
+//listen for the button to be clicked then runs call to jason and execute desired results
 var changeColorButton = document.getElementById("changeColorButton");
 changeColorButton.addEventListener("click", changeColor);
-
+//gets Json Data and translates it to text dependent on object and array specifications
 function changeColor() {
 	Request = new XMLHttpRequest();
 	Request.open('Get', 'changes.json');
 	Request.onload = function() {
 		var changes = JSON.parse(Request.responseText);
+		//sends and recives data to JSon to display desired results, it changes dependent on value of counter
 		document.getElementById("content").style.color = changes[0].colors[counter];
 
 	};
+	//resets value of counter based on array length (had to put in manually)
 	if(counter === 3) {
 		counter = 0;
 
@@ -21,17 +25,19 @@ function changeColor() {
 	}
 	Request.send();
 }
-
+//listen for the button to be clicked then runs call to jason and execute desired results
 document.getElementById("changeFontButton").addEventListener("click", changeFont);
-
+//gets Json Data and translates it to text dependent on object and array specifications
 function changeFont() {
 	Request = new XMLHttpRequest();
 	Request.open('Get', 'changes.json');
 	Request.onload = function() {
 		var changes = JSON.parse(Request.responseText);
+		//sends and recives data to JSon to display desired results, it changes dependent on value of counter
 		document.getElementById("content").style.fontFamily = changes[0].fontFamilies[counterS];
 
 	};
+	//resets value of counter based on array length (had to put in manually)
 	if(counterS === 3) {
 		counterS = 0;
 
@@ -41,7 +47,7 @@ function changeFont() {
 	}
 	Request.send();
 }
-
+// replaces text on call of function
 function newName() {
 	document.getElementById("name").innerHTML = document.getElementById("newName").value;
 
